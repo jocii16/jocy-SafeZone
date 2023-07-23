66658ce8c8d7abb3401d7a4cd6570012897eac90
JocY <109790602+JocY16@users.noreply.github.com>
2023-07-23 18:20:43 +0200

    client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,65 +1,28 @@
-local inZone = false
-
-function onExit(self)
-    inZone = true
-    lib.notify({
-        title = Config.TitleHaKilep,
-        description = Config.DescriptionHaKilep,
-        type = Config.TypeHaKilep,
-        position = 'top'
-    })
-    local playerPed = PlayerPedId()
-    local vehicle = GetVehiclePedIsIn(player, false)
-
-    SetEntityInvincible(playerPed, true)
-    NetworkSetFriendlyFireOption(false)
-	ClearPlayerWantedLevel(PlayerPedId())
-    SetEntityCanBeDamaged(vehicle, true)
-
-CreateThread(function ()
-    while true do
-        for k, v in ipairs(Config.zones) do
-            if cache.vehicle and inZone == true then
-                SetVehicleMaxSpeed(cache.vehicle, v.speedLimit)
-                Wait(50)
-            else
-                Wait(1000)
-            end 
-        end
-
-        if IsControlJustPressed(0, 106) and inZone then
-            SetCurrentPedWeapon(PlayerPedId(),GetHashKey("WEAPON_UNARMED"),true)
-        end
-    end 
-end)
-    
-end
-
-function onEnter(self)
-    inZone = false
-    lib.notify({
-        title = Config.TitleHaBelep,
-        description = Config.DescriptionHaBelep,
-        type = Config.TypeHaBelep,
-        position = 'top'
-    })
-    local playerPed = PlayerPedId()
-    local vehicle = GetVehiclePedIsIn(player, false)
-
-    SetEntityInvincible(playerPed, false)
-    NetworkSetFriendlyFireOption(true)
-    SetEntityCanBeDamaged(vehicle, true)
-
-end
-
-CreateThread(function ()
-    for k,v in pairs(Config.zones) do
+CreateThread(function()
+    for k, v in pairs(Config.zones) do
         local zone = lib.zones.sphere({
             coords = v.coords,
             radius = v.radius,
             debug = v.debug,
-            onEnter = onEnter,
-            onExit = onExit
+            inside = function()
+                SetEntityInvincible(cache.ped, false)
+                NetworkSetFriendlyFireOption(true)
+                SetEntityCanBeDamaged(cache.vehicle, true)
+            end
         })
+        local blip1 = AddBlipForCoord(v.coords)
+
+        SetBlipSprite(blip1, 487)
+        SetBlipColour(blip1, 45)
+        SetBlipScale(blip1, 1.2)
+        SetBlipAsShortRange(blip1, true)
+
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(v.blip)
+        EndTextCommandSetBlipName(blip1)
+
+        local blip3 = AddBlipForRadius(v.coords, v.radius)
+        SetBlipColour(blip3, 69)
+        SetBlipAlpha(blip3, 150)
     end
 end)
