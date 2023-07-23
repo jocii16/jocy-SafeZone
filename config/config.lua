0230efd40d0597fa438b548feb5399eb920d7103
JocY <109790602+JocY16@users.noreply.github.com>
2023-07-23 18:20:55 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -2,24 +2,8 @@ Config = {}
 
 Config.zones = {
     {
-    coords = vec3(2137.4287, 4778.1992, 40.9703), -- Position
-    radius = 20, --Size
+    coords = vec3(1147.5449, 124.1135, 81.9833), -- Position
+    radius = 20.0, --Size
     debug = true, -- Visibility of red outline (Recommended when making a new circle)
-    speedlimit = 4.0
-    },
-    -- {
-    -- coords = vec3(1949.5500, 4855.2549, 45.3800),
-    -- radius = 40,
-    -- debug = false
-    -- }
+    }
 }
-
--- If the player enters the zone, this is what notify
-Config.TitleHaBelep = 'Beléptél a SafeZoneba!'
-Config.DescriptionHaBelep = 'JocY - SafeZone'
-Config.TypeHaBelep = 'success'
-
--- If the player leaves the zone, this is what notify
-Config.TitleHaKilep = 'Kiléptél a SafeZoneból!'
-Config.DescriptionHaKilep = 'JocY - SafeZone'
-Config.TypeHaKilep = 'error'
